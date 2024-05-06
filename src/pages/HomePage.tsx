200c1deb275636952fbb5f116bef512031d84b64
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-05-06 19:19:38 +0530

    Updated HomePage for build

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,5 @@
 import CoinsTable from "../components/CoinsTable";
-import Header from "../components/Header";
 import Banner from "../components/banner/Banner";
-import CryptoContext from "../contexts/CryptoContext";
 
 const HomePage = () => {
       return (
